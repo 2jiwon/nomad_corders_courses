2092c08b9a23e900652768033cf32b9ac10d3640
이지원 <ljwjulian@gmail.com>
2022-03-20 18:48:11 +0900

    go 프로젝트 폴더 생성하고 코드 작성

---
+++ go_projects/project_1/accounts/accounts.go
@@ -0,0 +1,14 @@
+package accounts
+
+// Account structure
+type Account struct {
+	owner string
+	balance int
+}
+
+// NewAccount creates Account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+
+	return &account
+}
