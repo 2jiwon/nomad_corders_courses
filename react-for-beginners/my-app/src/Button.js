0f18695c03035caaa1317eba00552e5cb647b16f
2jiwon <ljwjulian@gmail.com>
2022-05-01 18:05:40 +0900

    버튼 스타일을 위한 module.css의 사용

--- react-for-beginners/my-app/src/Button.js
+++ react-for-beginners/my-app/src/Button.js
@@ -1,12 +1,11 @@
+import React from 'react';
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 function Button({ text }) {
     return (
         <button
-            style={{
-                backgroundColor: "tomato",
-                color: "white",
-            }}
+            className={styles.btn}
         >
             {text}
         </button>
