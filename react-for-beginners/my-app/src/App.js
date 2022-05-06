61850b09daaaec4957b69fc59e6848dec922e984
2jiwon <ljwjulian@gmail.com>
2022-05-06 16:01:28 +0900

    useEffect 사용예를 추가

--- react-for-beginners/my-app/src/App.js
+++ react-for-beginners/my-app/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, [keyword]);
   return (
     <div>
+      <input type="text" placeholder='Search...' onChange={onChange} value={keyword} />
       <h1 className={styles.title}>Welcome Back!</h1>
       <Button text={"Continue"} />
     </div>
