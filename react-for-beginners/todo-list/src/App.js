8d47ddf87929559f04a06ec5ac974ada1b014c38
2jiwon <ljwjulian@gmail.com>
2022-05-15 14:28:47 +0900

    todo input과 add button을 만들어 동작하도록 만듬

--- react-for-beginners/todo-list/src/App.js
+++ react-for-beginners/todo-list/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    console.log(todo);
+    setTodo("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={todo} placeholder="Write your to do..." onChange={onChange} />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
