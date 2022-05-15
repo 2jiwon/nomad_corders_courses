a8f551471ec498e903289757215ff4f2388dde34
2jiwon <ljwjulian@gmail.com>
2022-05-15 14:43:45 +0900

    todo를 입력하면 todos 배열에 추가되도록 함

--- react-for-beginners/todo-list/src/App.js
+++ react-for-beginners/todo-list/src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
   const onChange = (event) => setTodo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
@@ -11,8 +12,10 @@ function App() {
       return;
     }
     console.log(todo);
+    setTodos((currentTodos) => [todo, ...currentTodos]);
     setTodo("");
   }
+  console.log(todos);
   return (
     <div>
       <form onSubmit={onSubmit}>
