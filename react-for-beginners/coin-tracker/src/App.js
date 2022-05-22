b92dfdce76b7c516c209ac6f66dd574ccbdc4b5b
2jiwon <ljwjulian@gmail.com>
2022-05-22 19:57:11 +0900

    dUSD를 입력하면선택한 coin을 몇 개를 살 수 있는지 변환해서 알려줌

--- react-for-beginners/coin-tracker/src/App.js
+++ react-for-beginners/coin-tracker/src/App.js
@@ -3,6 +3,20 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [dollars, setDollars] = useState(0);
+  const [converted, setConverted] = useState([0, ""]);
+  let selected = [];
+  const onChange = (event) => {
+    setDollars(event.target.value);
+    setConverted([
+      selected.length == 0 ? 0 : (dollars / selected[0]).toFixed(2),
+      selected[1]
+    ]);
+  }
+  const onSelected = (event) => {
+    selected = event.target.value.split(',');
+    setConverted([(dollars / selected[0]).toFixed(2), selected[1]]);
+  }
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
       .then((response) => response.json())
@@ -15,9 +29,19 @@ function App() {
     <div>
       <h1>The Coins!</h1>
       {loading ? <strong>Loading...</strong> :
-        <ul>
-          {coins.map((coin) => <li>{coin.name} ({coin.symbol}) ${coin.quotes.USD.price} USD</li>)}
-        </ul>}
+        <select onChange={onSelected}>
+          <option>Please select a coin</option>
+          {coins.map((coin, index) =>
+            <option
+              key={index}
+              value={[coin.quotes.USD.price, coin.symbol]}>{coin.name} ({coin.symbol}) ${coin.quotes.USD.price} USD
+            </option>)}
+        </select>}
+
+      <div>
+        <input type="number" placeholder="dollars" value={dollars} onChange={onChange} /> USD
+        <h3>{converted[0]} {converted[1]}</h3>
+      </div>
     </div>
   );
 }
