f27168ead64ac6ebdba39f14c4ffafd84c9c613c
이지원 <ljwjulian@gmail.com>
2022-05-31 09:44:16 +0900

    go project 추가

--- go_projects/project_1/accounts/accounts.go
+++ go_projects/project_1/accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 // Account structure
 type Account struct {
 	owner string
@@ -16,6 +18,15 @@ func NewAccount(owner string) *Account {
 // Deposit x amount on your account
 func (a *Account) Deposit(amount int) {
 	a.balance += amount
+}
+// Withdraw x amount on your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errors.New("Can't withdraw this amount.")
+	}
+	a.balance -= amount
+
+	return nil
 }
 
 // Get the balance of the account
--- go_projects/project_1/main.go
+++ go_projects/project_1/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
-
-	"./accounts"
+	"project_1/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("jiwon")
 	account.Deposit(100)
+	err := account.Withdraw(200)
+	if err != nil {
+		//log.Fatalln(err)
+		fmt.Println(err)
+	}
 	fmt.Println(account.Balance())
 }
