590bc1b199436286cb77e1e1f1ff6af31ef94c5c
2jiwon <ljwjulian@gmail.com>
2022-06-01 17:16:07 +0900

    await를 좀더 간단하게

--- react-for-beginners/movie-app/src/App.js
+++ react-for-beginners/movie-app/src/App.js
@@ -4,8 +4,9 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year");
-    const json = await response.json();
+    const json = await (
+      await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
+    ).json();
     setMovies(json.data.movies);
     setLoading(true);
   };
