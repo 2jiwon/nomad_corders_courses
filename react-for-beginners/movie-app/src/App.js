64e50619b5818251ba4d6d80f8157ae3ea0cb7d2
2jiwon <ljwjulian@gmail.com>
2022-06-01 17:11:40 +0900

    then 대신 async await 사용

--- react-for-beginners/movie-app/src/App.js
+++ react-for-beginners/movie-app/src/App.js
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [loading, setLoading] = useState(false);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year");
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(true);
+  };
   useEffect(() => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(() => json.data.movies);
-        setLoading(true);
-      });
+    getMovies();
   }, []);
   console.log(movies);
   return (
