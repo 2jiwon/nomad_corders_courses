bf5b54e12c4464c2deeec710c01a5596978e6e3b
2jiwon <ljwjulian@gmail.com>
2022-06-01 17:08:01 +0900

    movies에 data를 넣어줌

--- react-for-beginners/movie-app/src/App.js
+++ react-for-beginners/movie-app/src/App.js
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
+        setMovies(() => json.data.movies);
         setLoading(true);
       });
   }, []);
+  console.log(movies);
   return (
     <div>
       {loading ? <h2>Loading...</h2> : null}
