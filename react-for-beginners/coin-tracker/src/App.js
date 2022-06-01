5e97d1de0bc6760c95f1f3bfb625c9a6f3af3830
2jiwon <ljwjulian@gmail.com>
2022-06-01 16:12:20 +0900

    선택한 coin을 입력한 USD로 변환해서 보여주도록 완성

--- react-for-beginners/coin-tracker/src/App.js
+++ react-for-beginners/coin-tracker/src/App.js
@@ -5,18 +5,19 @@ function App() {
   const [coins, setCoins] = useState([]);
   const [dollars, setDollars] = useState(0);
   const [converted, setConverted] = useState([0, ""]);
-  let selected = [];
+  const [selectedCoin, setSelected] = useState("");
+  const [symbol, setSymbol] = useState("");
   const onChange = (event) => {
     setDollars(event.target.value);
-    setConverted([
-      selected.length == 0 ? 0 : (dollars / selected[0]).toFixed(2),
-      selected[1]
-    ]);
   }
-  const onSelected = (event) => {
-    selected = event.target.value.split(',');
-    setConverted([(dollars / selected[0]).toFixed(2), selected[1]]);
+  const onSelectChange = (event) => {
+    var index = event.target.selectedIndex;
+    setSelected(() => event.target.value);
+    setSymbol(() => event.target.childNodes[index].getAttribute('name'));
   }
+  useEffect(() => {
+    setConverted([(dollars / selectedCoin).toFixed(2), symbol]);
+  }, [dollars, selectedCoin, symbol]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
       .then((response) => response.json())
@@ -29,18 +30,19 @@ function App() {
     <div>
       <h1>The Coins!</h1>
       {loading ? <strong>Loading...</strong> :
-        <select onChange={onSelected}>
+        <select value={selectedCoin} onChange={onSelectChange}>
           <option>Please select a coin</option>
           {coins.map((coin, index) =>
             <option
               key={index}
-              value={[coin.quotes.USD.price, coin.symbol]}>{coin.name} ({coin.symbol}) ${coin.quotes.USD.price} USD
+              name={coin.symbol}
+              value={coin.quotes.USD.price}>{coin.name} ({coin.symbol}) ${coin.quotes.USD.price} USD
             </option>)}
         </select>}
 
       <div>
         <input type="number" placeholder="dollars" value={dollars} onChange={onChange} /> USD
-        <h3>{converted[0]} {converted[1]}</h3>
+        {!isNaN(converted[0]) ? <h3>{converted[0]} {converted[1]}</h3> : null}
       </div>
     </div>
   );
