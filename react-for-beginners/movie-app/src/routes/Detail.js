368120c089d8db17fd55b3619b3c8f701aa573c1
2jiwon <ljwjulian@gmail.com>
2022-06-05 16:29:20 +0900

    Detail을 출력해봄

--- react-for-beginners/movie-app/src/routes/Detail.js
+++ react-for-beginners/movie-app/src/routes/Detail.js
@@ -1,20 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
     const { id } = useParams();
-    //console.log(id);
+    const [loading, setLoading] = useState(false);
+    const [detail, setDetail] = useState({});
     const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
         console.log(json);
+        setLoading(true);
+        setDetail(json.data.movie);
     };
     useEffect(() => {
         getMovie();
     }, []);
     return (
-        <h1>Detail</h1>
+        <div>
+            {loading ? <div>
+                <h2>{detail.title_long}</h2>
+                <img src={detail.medium_cover_image} />
+                <p>Rating: {detail.rating}</p>
+                <p>Runtime : {detail.runtime} minutes</p>
+                <p>Description: {detail.description_full}</p>
+            </div> : <h1>Loading...</h1>}
+        </div>
     )
 }
 export default Detail;
