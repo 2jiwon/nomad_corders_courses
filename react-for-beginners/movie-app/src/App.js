eb864e8d15907546da21973b7267d28d76459743
2jiwon <ljwjulian@gmail.com>
2022-06-05 15:06:10 +0900

    react-router-dom 설치, Router를 사용해서 render하도록 변경

--- react-for-beginners/movie-app/src/App.js
+++ react-for-beginners/movie-app/src/App.js
@@ -1,42 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import Movie from './Movie';
-import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './routes/Home';
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(true);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ?
-        <div>{movies.map((movie) =>
-          <Movie id={movie.id}
-            title={movie.title}
-            coverImg={movie.medium_cover_image}
-            genres={movie.genres}
-            summary={movie.summary} />
-        )}
-        </div> : <h2>Loading...</h2>
-      }
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      </Route>
+    </Routes>
+  </Router>
 }
-
-Movie.PropTypes = {
-  coverImg = PropTypes.string.isRequired,
-  title = PropTypes.string.isRequired,
-  summary = PropTypes.string.isRequired,
-  genres = PropTypes.arrayOf(PropTypes.string).isRequired,
-}
-
 export default App;
---
+++ react-for-beginners/movie-app/src/routes/Home.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import Movie from '../components/Movie';
+import PropTypes from 'prop-types';
+
+function Home() {
+    const [loading, setLoading] = useState(false);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const json = await (
+            await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
+        ).json();
+        setMovies(json.data.movies);
+        setLoading(true);
+    };
+    useEffect(() => {
+        getMovies();
+    }, []);
+    console.log(movies);
+    return (
+        <div>
+            {loading ?
+                <div>{movies.map((movie) =>
+                    <Movie key={movie.id}
+                        title={movie.title}
+                        coverImg={movie.medium_cover_image}
+                        genres={movie.genres}
+                        summary={movie.summary} />
+                )}
+                </div> : <h2>Loading...</h2>
+            }
+        </div>
+    );
+}
+
+Movie.propTypes = {
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Home;
