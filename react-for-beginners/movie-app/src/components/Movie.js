5558e422f57182f15c0cd2d4e84693684a03db8d
2jiwon <ljwjulian@gmail.com>
2022-06-05 15:24:42 +0900

    Home, Movie router 추가하고, Detail을 보여주도록 함

--- react-for-beginners/movie-app/src/components/Movie.js
+++ react-for-beginners/movie-app/src/components/Movie.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function Movie({ title, coverImg, genres, summary }) {
     return (
         <div>
-            <h2>{title}</h2>
+            <h2>
+                <Link to="/movie">{title}</Link>
+            </h2>
             <img src={coverImg} />
             <ul>
                 {genres.map((genre) => <li key={genre}>{genre}</li>)}
