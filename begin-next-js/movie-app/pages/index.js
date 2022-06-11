027dea40dd83c8b10be652ace9f5c4a95710575f
2jiwon <ljwjulian@gmail.com>
2022-06-11 16:00:16 +0900

    가져온 data를 화면에 출력

--- begin-next-js/movie-app/pages/index.js
+++ begin-next-js/movie-app/pages/index.js
@@ -7,16 +7,22 @@ export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const data = await (
+      const { results } = await (
         await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
       ).json();
-      console.log(data);
+      //console.log(results);
+      setMovies(results);
     })();
   }, [])
   return (
     <div>
       <Seo title="Home" />
-      <h1>This is Home</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
