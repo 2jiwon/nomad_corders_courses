8e45affcb69eb8ba74c950b432a45780cab7d5ed
2jiwon <ljwjulian@gmail.com>
2022-06-11 15:23:57 +0900

    themoviedb의 api를 사용해서 movie data를 받아와서 콘솔에 출력해봄

--- begin-next-js/movie-app/pages/index.js
+++ begin-next-js/movie-app/pages/index.js
@@ -1,6 +1,17 @@
 import Seo from "../components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "4a638c7233d032a392b46401705afddf";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+      const json = await response.json();
+      console.log(json);
+    })();
+  }, [])
   return (
     <div>
       <Seo title="Home" />
