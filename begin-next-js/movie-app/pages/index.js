fbd8cb0e852bc33a05ab8ca001c9638f0ae02860
2jiwon <ljwjulian@gmail.com>
2022-06-11 14:59:18 +0900

    head 내용을 Seo 컴포넌트로 만들어서 각 페이지에서 import

--- begin-next-js/movie-app/pages/index.js
+++ begin-next-js/movie-app/pages/index.js
@@ -1,11 +1,9 @@
-import Head from "next/head";
+import Seo from "../components/Seo";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Home | Next Movies</title>
-      </Head>
+      <Seo title="Home" />
       <h1>This is Home</h1>
     </div>
   )
