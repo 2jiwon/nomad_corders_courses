496b0a46103ff9de337262fb0c75c049decf92b5
2jiwon <ljwjulian@gmail.com>
2022-06-11 15:35:49 +0900

    async await를 사용

--- begin-next-js/movie-app/pages/index.js
+++ begin-next-js/movie-app/pages/index.js
@@ -7,9 +7,10 @@ export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
-      const json = await response.json();
-      console.log(json);
+      const data = await (
+        await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+      ).json();
+      console.log(data);
     })();
   }, [])
   return (
