14e8a0c2e9fb9d9b5fe1356f5de482470d595b35
2jiwon <ljwjulian@gmail.com>
2022-06-11 14:45:35 +0900

    Layout pattern을 사용

--- begin-next-js/movie-app/pages/_app.js
+++ begin-next-js/movie-app/pages/_app.js
@@ -1,10 +1,8 @@
-import NavBar from "../components/NavBar"
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
     return (
         <div>
-            <NavBar />
             <Component {...pageProps} />
             <span>Hello</span>
         </div>
