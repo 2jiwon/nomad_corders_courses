e35fb98579b49e795472a6d1323f69ea65b33ce6
2jiwon <ljwjulian@gmail.com>
2022-06-11 16:16:39 +0900

    rewrites 기능을 사용해서 api fetch 주소를 변경, API_KEY를 env로 분리

--- begin-next-js/movie-app/next.config.js
+++ begin-next-js/movie-app/next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
+
+  async rewrites() {
     return [
       {
-        source: "/contact",
-        destination: "/form",
-        permanent: false,
-      }
-    ]
-  }
-}
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
--- begin-next-js/movie-app/pages/index.js
+++ begin-next-js/movie-app/pages/index.js
@@ -1,14 +1,12 @@
 import Seo from "../components/Seo";
 import { useEffect, useState } from "react";
 
-const API_KEY = "4a638c7233d032a392b46401705afddf";
-
 export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
       const { results } = await (
-        await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+        await fetch("/api/movies")
       ).json();
       //console.log(results);
       setMovies(results);
