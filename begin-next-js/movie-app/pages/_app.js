ab88dd3d7191e6c944100ffac88aa61a7a2e9bc3
2jiwon <ljwjulian@gmail.com>
2022-06-11 14:38:24 +0900

    _app.js로 NavBar를 이동하여 페이지별로 import하지 않아도 되도록

--- begin-next-js/movie-app/pages/_app.js
+++ begin-next-js/movie-app/pages/_app.js
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar"
+
 export default function App({ Component, pageProps }) {
     return (
         <div>
+            <NavBar />
             <Component {...pageProps} />
             <span>Hello</span>
         </div>
--- begin-next-js/movie-app/pages/index.js
+++ begin-next-js/movie-app/pages/index.js
@@ -1,9 +1,6 @@
-import NavBar from "../components/NavBar"
-
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>This is Home</h1>
     </div>
   )
