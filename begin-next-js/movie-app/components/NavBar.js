af105564bbe9e65dbee8f1b3ecb624eb4ae8d447
2jiwon <ljwjulian@gmail.com>
2022-06-11 13:56:43 +0900

    useRouter를 추가하고 값을 확인해봄

--- begin-next-js/movie-app/components/NavBar.js
+++ begin-next-js/movie-app/components/NavBar.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+    const router = useRouter();
+    console.log(router);
     return (
         <nav>
             <Link href="/">
