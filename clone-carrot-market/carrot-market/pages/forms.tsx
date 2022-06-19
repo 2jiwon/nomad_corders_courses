c12684d2cd3399a00748b07bb1918de5ba150ec8
2jiwon <ljwjulian@gmail.com>
2022-06-19 17:36:33 +0900

    react-hook-form을 사용, register 함수로 input을 등록

--- clone-carrot-market/carrot-market/pages/forms.tsx
+++ clone-carrot-market/carrot-market/pages/forms.tsx
@@ -1,7 +1,16 @@
+import { useForm } from "react-hook-form";
+
 export default function Forms() {
+  const { register } = useForm();
+  console.log(register("banana"));
   return (
     <form>
-      <input type="text" placeholder="Username" required />
+      <input
+        {...register("username")}
+        type="text"
+        placeholder="Username"
+        required
+      />
       <input type="email" placeholder="Email" required />
       <input type="password" placeholder="Password" required />
       <input type="submit" value="Create Account" />
