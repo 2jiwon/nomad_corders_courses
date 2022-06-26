5de52a0bd082665c77fe47f01cfde9130249c85f
2jiwon <ljwjulian@gmail.com>
2022-06-26 15:44:59 +0900

    valid에 data type, invalid에 errors type을 추가하고 상세한 조건도 추가

--- clone-carrot-market/carrot-market/pages/forms.tsx
+++ clone-carrot-market/carrot-market/pages/forms.tsx
@@ -1,35 +1,45 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
     console.log("I'm valid");
   };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...(register("username"),
-        {
-          required: true,
+        {...register("username", {
+          required: "Username is required.",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
         })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...(register("email"),
-        {
-          required: true,
-        })}
-        type="email"
-        placeholder="Email"
-      />
-      <input
-        {...(register("password"),
-        {
-          required: true,
+        {...register("password", {
+          required: "Password is requried.",
         })}
         type="password"
         placeholder="Password"
+      />
+      <input
+        {...register("email", {
+          required: "Email is required.",
+        })}
+        type="email"
+        placeholder="Email"
       />
       <input type="submit" value="Create Account" />
     </form>
