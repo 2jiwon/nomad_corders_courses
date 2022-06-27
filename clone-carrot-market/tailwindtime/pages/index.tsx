4b566a90aeefcb7b89499aa42a01969645708a55
이지원 <ljwjulian@gmail.com>
2022-06-27 17:57:49 +0900

    두번째 div 작성

--- clone-carrot-market/tailwindtime/pages/index.tsx
+++ clone-carrot-market/tailwindtime/pages/index.tsx
@@ -4,11 +4,12 @@ const Home: NextPage = () => {
   return (
     <div className='bg-fuchsia-100 min-h-screen mx-auto font-sans'>
       <div className='grid sm:grid-cols-2 md:grid-cols-3  gap-1 mx-16 min-h-screen py-20 place-items-center'>
+
+        {/* 첫번째 div */}
         <div className='bg-white w-4/5 h-full px-5 pt-10'>
-          <h1 className='text-4xl font-extrabold'>Weather</h1>
+          <h1 className='text-5xl font-extrabold'>Weather</h1>
           
           <div className='grid grid-rows-4 gap-8 mt-10'>
-
             <div className='border-black border-solid border-2 border-b-8 rounded-xl grid sm:grid-cols-1 md:grid-cols-3 p-5'>
               <div>
                 <h1 className='font-extrabold'>Casius</h1>
@@ -68,19 +69,56 @@ const Home: NextPage = () => {
                 <h1 className='text-4xl font-extrabold'>85°</h1>
               </div>
             </div>
-
           </div>
 
           <div className='mx-auto my-20 border-black border-solid border-2 border-b-4 rounded-full w-14 h-14'>
             <h1 className='text-4xl text-center mt-1'>+</h1>
           </div>
         </div>
+        {/* 첫번째 div End */}
 
-        <div className='bg-white w-4/5 h-full px-5 pt-10'>
-          <div>X</div>
+        <div className='bg-white w-4/5 h-full px-5'>
+          <div className='my-10 border-black border-solid border-2 border-b-4 rounded-full w-14 h-14'>
+            <h1 className='text-4xl text-center mt-1'>X</h1>
+          </div>
           <div>
-            <h3>SIMPLE TAG</h3>
-
+            <div className='-my-10'>
+              <h3 className='text-center font-extrabold text-lg'>SIMPLE TAG</h3>
+              <h1 className='pt-5 text-center font-extrabold text-5xl'>Work with best designers</h1>
+            </div>
+          </div>
+          <div className='my-10 grid grid-cols-2 grid-rows-2 gap-2 pt-10 place-items-center'>
+            <div className='border-black border-solid border-2 w-40 h-44 rounded-xl bg-yellow-400'>
+              <div className='px-12 py-14'>
+                <svg className="w-14 h-14 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                </svg>
+              </div>
+            </div>
+            <div className='border-black border-solid border-2 w-40 h-44 rounded-xl bg-teal-400'>
+              <div className='px-12 py-14'>
+                <svg className="w-14 h-14 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                </svg>
+              </div>
+            </div>
+            <div className='border-black border-solid border-2 w-40 h-44 rounded-xl bg-pink-400'>
+              <div className='px-12 py-14'>
+                <svg className="w-14 h-14 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                </svg>
+              </div>
+            </div>
+            <div className='border-black border-solid border-2 w-40 h-44 rounded-xl bg-red-400'>
+              <div className='px-12 py-14'>
+                <svg className="w-14 h-14 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className='justify-items-center'>
+            <div className='bg-blue-500 rounded-lg text-center text-white font-extrabold align-middle py-3'>Let's get it done</div>
           </div>
         </div>
 
