5281f9201964b3fc446f1372f2186b366c10895f
이지원 <ljwjulian@gmail.com>
2022-06-28 17:16:02 +0900

    스타일 수정

--- clone-carrot-market/tailwindtime/pages/index.tsx
+++ clone-carrot-market/tailwindtime/pages/index.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-fuchsia-100 min-w-fit min-h-screen mx-auto font-sans">
+    <div className="bg-fuchsia-100 w-auto min-h-screen mx-auto font-sans">
       {/* div들 container */}
       <div className="grid xs:grid-cols-1 lg:grid-cols-3 gap-1 mx-16 min-h-screen py-20 place-items-center">
         {/* 첫번째 div */}
-        <div className="bg-white w-4/5 h-full px-5 pt-10 md:mr-0 lg:-mr-32">
+        <div className="bg-white w-5/6 h-full px-5 pt-10 md:mr-0 lg:-mr-32">
           <h1 className="text-5xl font-extrabold">Weather</h1>
 
           <div className="grid grid-rows-4 gap-8 mt-10">
@@ -78,51 +78,49 @@ const Home: NextPage = () => {
         {/* 첫번째 div End */}
 
         {/* 두번째 div */}
-        <div className="bg-white w-4/5 h-full px-5">
+        <div className="bg-white w-5/6 h-full px-5">
           <div className="my-10 border-black border-solid border-2 border-b-4 rounded-full w-14 h-14">
-            <h1 className="text-4xl text-center mt-1">X</h1>
+            <h1 className="text-3xl text-center font-bold my-2">X</h1>
           </div>
-          <div>
-            <div className="-my-10">
-              <h3 className="text-gray-600 text-center font-extrabold text-lg">
-                SIMPLE TAG
-              </h3>
-              <h1 className="pt-5 text-center font-extrabold text-4xl">
-                Work with best designers
-              </h1>
-            </div>
+          <div className="my-3">
+            <h3 className="text-gray-600 text-center font-extrabold text-lg">
+              SIMPLE TAG
+            </h3>
+            <h1 className="pt-5 text-center font-extrabold text-4xl">
+              Work with best designers
+            </h1>
           </div>
-          <div className="my-10 grid sm:grid-cols-1 lg:grid-cols-2 xs:grid-rows-4 lg:grid-rows-2 gap-4 pt-10 place-items-center">
-            <div className="border-black border-solid border-2 w-40 h-44 rounded-xl bg-yellow-400 xs:mr-0 lg:-mr-4">
-              <div className="px-12 py-14">
+          <div className="my-10 grid sm:grid-cols-1 lg:grid-cols-2 xs:grid-rows-4 lg:grid-rows-2 gap-4 place-items-center">
+            <div className="border-black border-solid border-2 w-44 h-48 rounded-xl bg-yellow-400 lg:place-self-end">
+              <div className="px-14 py-16">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
               </div>
             </div>
-            <div className="border-black border-solid border-2 w-40 h-44 rounded-xl bg-teal-400 xs:mr-0 lg:mr-4">
-              <div className="px-12 py-14">
+            <div className="border-black border-solid border-2 w-44 h-48 rounded-xl bg-teal-400 lg:place-self-start">
+              <div className="px-14 py-16">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
               </div>
             </div>
-            <div className="border-black border-solid border-2 w-40 h-44 rounded-xl bg-pink-400 xs:mr-0 lg:-mr-4">
-              <div className="px-12 py-14">
+            <div className="border-black border-solid border-2 w-44 h-48 rounded-xl bg-pink-400 lg:place-self-end">
+              <div className="px-14 py-16">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
               </div>
             </div>
-            <div className="border-black border-solid border-2 w-40 h-44 rounded-xl bg-red-400 xs:mr-0 lg:mr-4">
-              <div className="px-12 py-14">
+            <div className="border-black border-solid border-2 w-44 h-48 rounded-xl bg-red-400 lg:place-self-start">
+              <div className="px-14 py-16">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
               </div>
             </div>
           </div>
-          <div className="grid grid-rows-1 place-items-center mx-4">
+          <div className="grid grid-rows-1 place-items-center mx-4 my-5">
             <button className="bg-blue-700 rounded-lg text-center w-full text-white font-extrabold align-middle py-3">
               Let's get it done
             </button>
@@ -131,17 +129,17 @@ const Home: NextPage = () => {
         {/* 두번째 div End */}
 
         {/* 세번째 div */}
-        <div className="bg-yellow-400 w-4/5 h-full px-5 md:mr-0 lg:mr-32">
+        <div className="bg-yellow-400 w-5/6 h-full px-5 md:mr-0 lg:mr-32">
           <div className="grid grid-cols-3 place-content-center">
             <div className="bg-white my-10 border-black border-solid border-2 border-b-4 rounded-full w-14 h-14">
-              <h1 className="text-4xl text-center mt-1">〈</h1>
+              <h1 className="text-3xl text-center font-bold my-2">〈</h1>
             </div>
             <div className="place-self-center">
               <h1 className="text-center font-extrabold text-3xl">Friends</h1>
             </div>
           </div>
 
-          <div className="grid xs:grid-rows-6 gap-2">
+          <div className="grid xs:grid-rows-6 gap-3 my-4">
             <div className="relative">
               <input
                 className="w-full h-16 bg-white rounded-xl text-left text-xl px-5 border-black border-solid border-2 border-b-8 
@@ -169,7 +167,7 @@ const Home: NextPage = () => {
                   <p className="font-montserrat font-semibold">Planet Designer</p>
                 </div>
                 <div className="xl:place-self-end">
-                  <button className="bg-yellow-400 w-24 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-xl font-extrabold">Invite</button>
+                  <button className="bg-yellow-400 w-20 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-md font-extrabold">Invite</button>
                 </div>
               </div>
             </div>
@@ -187,7 +185,7 @@ const Home: NextPage = () => {
                   <p className="font-montserrat font-semibold">Planet Designer</p>
                 </div>
                 <div className="xl:place-self-end">
-                  <button className="bg-yellow-400 w-24 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-xl font-extrabold">Invite</button>
+                  <button className="bg-yellow-400 w-20 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-md font-extrabold">Invite</button>
                 </div>
               </div>
             </div>
@@ -205,7 +203,7 @@ const Home: NextPage = () => {
                   <p className="font-montserrat font-semibold">Rogue Corp</p>
                 </div>
                 <div className="xl:place-self-end">
-                  <button className="bg-black w-24 text-white h-12 rounded-xl border-black border-solid border-2 border-b-4 text-xl font-extrabold">Invited</button>
+                  <button className="bg-black w-20 text-white h-12 rounded-xl border-black border-solid border-2 border-b-4 text-md font-extrabold">Invited</button>
                 </div>
               </div>
             </div>
@@ -223,7 +221,7 @@ const Home: NextPage = () => {
                   <p className="font-montserrat font-semibold">Hard Cops</p>
                 </div>
                 <div className="xl:place-self-end">
-                  <button className="bg-yellow-400 w-24 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-xl font-extrabold">Invite</button>
+                  <button className="bg-yellow-400 w-20 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-md font-extrabold">Invite</button>
                 </div>
               </div>
             </div>
@@ -241,7 +239,7 @@ const Home: NextPage = () => {
                   <p className="font-montserrat font-semibold">Xenon Creator</p>
                 </div>
                 <div className="xl:place-self-end">
-                  <button className="bg-yellow-400 w-24 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-xl font-extrabold">Invite</button>
+                  <button className="bg-yellow-400 w-20 h-12 rounded-xl border-black border-solid border-2 border-b-4 text-md font-extrabold">Invite</button>
                 </div>
               </div>
             </div>
