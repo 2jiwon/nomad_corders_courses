6681b56bbee16156d5273d81898945a65efc8a1f
2jiwon <ljwjulian@gmail.com>
2022-07-06 18:47:12 +0900

    사용자 정보가 있으면 가져오고, 없으면 새로 만들도록 함

--- clone-carrot-market/carrot-market/pages/api/users/enter.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/enter.tsx
@@ -1,8 +1,48 @@
+import client from "@libs/client/client";
 import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  let user;
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      console.log("Did not find. Will create one.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+
+    if (!user) {
+      console.log("Did not find. Will create one.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  }
+
   return res.status(200).end();
 }
 
