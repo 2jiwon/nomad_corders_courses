952280c6f69974bafec1b3bceaad39a9a52e7c25
이지원 <ljwjulian@gmail.com>
2022-07-06 12:36:09 +0900

    withHandler 함수 작성

--- clone-carrot-market/carrot-market/pages/api/users/enter.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/enter.tsx
@@ -1,12 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/client/client";
+import withHandler from "../../../libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
   console.log(req.body);
-  res.json({ ok: true });
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
