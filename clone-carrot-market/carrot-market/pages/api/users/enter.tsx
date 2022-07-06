80b4b0e5513636368e543a9320ae787091772e08
2jiwon <ljwjulian@gmail.com>
2022-07-06 18:56:45 +0900

    반복되는 코드 대신 ES6 문법을 활용해서 줄임

--- clone-carrot-market/carrot-market/pages/api/users/enter.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/enter.tsx
@@ -4,44 +4,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  let user;
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      console.log("Did not find. Will create one.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-
-    if (!user) {
-      console.log("Did not find. Will create one.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  }
+  const user = await client.user.upsert({
+    where: {
+      ...(phone && { phone: +phone }),
+      ...(email && { email }),
+    },
+    create: {
+      name: "Anonymous",
+      ...(phone && { phone: +phone }),
+      ...(email && { email }),
+    },
+    update: {},
+  });
+  console.log(user);
 
   return res.status(200).end();
 }
