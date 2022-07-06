6d3d1c25ec0bfa487014de5911a1bc0f5d8aae23
2jiwon <ljwjulian@gmail.com>
2022-07-06 19:47:33 +0900

    user 부분 코드가 필요없어졌으므로 제거

--- clone-carrot-market/carrot-market/pages/api/users/enter.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/enter.tsx
@@ -5,24 +5,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
-    },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
-  });
-  console.log(user);
-
   const token = await client.token.create({
     data: {
       payload: "1234",
       user: {
-        connect: {
-          id: user.id,
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
         },
       },
     },
