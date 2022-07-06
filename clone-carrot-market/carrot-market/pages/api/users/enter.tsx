dc8d549c0e8b4a8b38e352493a02528d6893d701
이지원 <ljwjulian@gmail.com>
2022-07-06 16:01:47 +0900

    paths를 수정

--- clone-carrot-market/carrot-market/pages/api/users/enter.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/enter.tsx
@@ -1,6 +1,5 @@
+import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/client/client";
-import withHandler from "../../../libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
