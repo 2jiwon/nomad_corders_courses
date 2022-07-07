3d256c339a33daea6bf1a5639d62042885cb8af4
이지원 <ljwjulian@gmail.com>
2022-07-07 17:30:08 +0900

    iron-session활용> 토큰으로 유저정보를 가져와서 쿠키로 저장

--- clone-carrot-market/carrot-market/pages/api/users/confirm.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/confirm.tsx
@@ -1,14 +1,30 @@
 import client from "@libs/client/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  console.log(req.session);
+
   const { token } = req.body;
-  console.log(token);
+  // console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "123456098123584956abcdefghijklmn",
+});
