f2610f398e5c205e4cd37827612612d2770eced1
2jiwon <ljwjulian@gmail.com>
2022-09-11 17:28:25 +0900

    handler를 호출하는 부분을 객체로 변경

--- clone-carrot-market/carrot-market/pages/api/users/me.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/me.tsx
@@ -20,4 +20,10 @@ async function handler(
   });
 }
 
-export default withApiSession(withHandler("GET", handler, true));
+export default withApiSession(
+  withHandler({
+    method: "GET",
+    handler,
+    isPrivate: true,
+  })
+);
