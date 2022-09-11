a0395ed37b70c57932084eea584871846a2f0c72
2jiwon <ljwjulian@gmail.com>
2022-09-11 15:57:43 +0900

    iron-session declare 부분도 withSesion으로 이동시킴

--- clone-carrot-market/carrot-market/pages/api/users/confirm.tsx
+++ clone-carrot-market/carrot-market/pages/api/users/confirm.tsx
@@ -3,14 +3,6 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import { withApiSession } from "@libs/server/withSession";
 
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
@@ -19,17 +11,22 @@ async function handler(
 
   const { token } = req.body;
   // console.log(token);
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
   });
-  if (!exists) return res.status(404).end();
+  if (!foundToken) return res.status(404).end();
   req.session.user = {
-    id: exists.userId,
+    id: foundToken.userId,
   };
   await req.session.save();
-  res.status(200).end();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+  res.json({ ok: true });
 }
 
 export default withApiSession(withHandler("POST", handler));
