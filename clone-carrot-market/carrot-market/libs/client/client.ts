17bf22e62b104ae194b5b0688dba7f5c01511ff4
이지원 <ljwjulian@gmail.com>
2022-09-27 16:40:08 +0900

    global client 관련 수정

--- clone-carrot-market/carrot-market/libs/client/client.ts
+++ clone-carrot-market/carrot-market/libs/client/client.ts
@@ -1,3 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+  var client: PrismaClient | undefined;
+}
+
+// global client를 client에 저장. 해당 값이 없으면 PrismaClient 생성
+const client = global.client || new PrismaClient();
+
+// 개발모드 일 때만
+if (process.env.NODE_ENV === "development") {
+  global.client = client;
+}
+
+export default client;
